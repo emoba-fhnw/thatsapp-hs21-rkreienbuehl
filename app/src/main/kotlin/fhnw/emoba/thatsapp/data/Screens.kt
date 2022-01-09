ad5ebf78083e9dacdfb60d4bf7f1fc7b478464dd
rogerkreienbuehl <r.kreienbuehl@gmx.ch>
2022-01-09 11:05:11 +0100

    feat: added route for screen

--- app/src/main/kotlin/fhnw/emoba/thatsapp/data/Screens.kt
+++ app/src/main/kotlin/fhnw/emoba/thatsapp/data/Screens.kt
@@ -6,8 +6,8 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
-enum class Screens(val title: String, val icon: ImageVector) {
-    CHATS("Chats", Icons.Filled.Chat),
-    USERS("Benutzer", Icons.Filled.Person),
-    SETTINGS("Einstellungen", Icons.Filled.Settings)
+enum class Screens(val title: String, val icon: ImageVector, val route: String) {
+    CHATS("Chats", Icons.Filled.Chat, "chatList"),
+    USERS("Benutzer", Icons.Filled.Person, "users"),
+    SETTINGS("Einstellungen", Icons.Filled.Settings, "settings")
 }
